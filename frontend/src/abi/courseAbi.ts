cba5a39575cb260f21cb59747e2e9c1a51b0f0d9
in-th3-l00p <indianucode0303@gmail.com>
2024-11-10 18:28:57 +0200

    showing user courses

--- frontend/src/abi/courseAbi.ts
+++ frontend/src/abi/courseAbi.ts
@@ -1,114 +1,387 @@
 const courseAbi = [
-  // Events
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: "string",
-        name: "oldTitle",
-        type: "string",
-      },
-      {
-        indexed: false,
-        internalType: "string",
-        name: "newTitle",
-        type: "string",
-      },
-    ],
-    name: "TitleUpdated",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "previousOwner",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
-    ],
-    name: "OwnershipTransferred",
-    type: "event",
-  },
-
-  // Functions
-  {
-    inputs: [],
-    name: "getTitle",
-    outputs: [
-      {
-        internalType: "string",
-        name: "",
-        type: "string",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "string",
-        name: "_newTitle",
-        type: "string",
-      },
-    ],
-    name: "setTitle",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "owner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "renounceOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
-    ],
-    name: "transferOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-
-  // Special Functions
-  {
-    stateMutability: "payable",
-    type: "receive",
-  },
-  {
-    stateMutability: "payable",
-    type: "fallback",
-  },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "internalType": "string",
+                "name": "_title",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "_slug",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "_description",
+                "type": "string"
+            },
+            {
+                "internalType": "string",
+                "name": "_category",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_price",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "owner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnableInvalidOwner",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "OwnableUnauthorizedAccount",
+        "type": "error"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "oldCategory",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "newCategory",
+                "type": "string"
+            }
+        ],
+        "name": "CategoryUpdated",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "oldDescription",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "newDescription",
+                "type": "string"
+            }
+        ],
+        "name": "DescriptionUpdated",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "oldPrice",
+                "type": "uint256"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "newPrice",
+                "type": "uint256"
+            }
+        ],
+        "name": "PriceUpdated",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "oldSlug",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "newSlug",
+                "type": "string"
+            }
+        ],
+        "name": "SlugUpdated",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "oldTitle",
+                "type": "string"
+            },
+            {
+                "indexed": false,
+                "internalType": "string",
+                "name": "newTitle",
+                "type": "string"
+            }
+        ],
+        "name": "TitleUpdated",
+        "type": "event"
+    },
+    {
+        "stateMutability": "payable",
+        "type": "fallback"
+    },
+    {
+        "inputs": [],
+        "name": "category",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "description",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "get",
+        "outputs": [
+            {
+                "components": [
+                    {
+                        "internalType": "string",
+                        "name": "title",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "slug",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "description",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "string",
+                        "name": "category",
+                        "type": "string"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "price",
+                        "type": "uint256"
+                    }
+                ],
+                "internalType": "struct Course.CourseDto",
+                "name": "",
+                "type": "tuple"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "price",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "renounceOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_category",
+                "type": "string"
+            }
+        ],
+        "name": "setCategory",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_description",
+                "type": "string"
+            }
+        ],
+        "name": "setDescription",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_price",
+                "type": "uint256"
+            }
+        ],
+        "name": "setPrice",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_slug",
+                "type": "string"
+            }
+        ],
+        "name": "setSlug",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_newTitle",
+                "type": "string"
+            }
+        ],
+        "name": "setTitle",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "slug",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "title",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "stateMutability": "payable",
+        "type": "receive"
+    }
 ];
 
 export default courseAbi;
--- frontend/src/components/dashboard/Course.tsx
+++ frontend/src/components/dashboard/Course.tsx
@@ -1,49 +1,98 @@
 import { useReadContract } from "wagmi";
 import courseAbi from "../../abi/courseAbi";
-import { DollarSign, Settings, Users } from "lucide-react";
+import { DollarSign, Settings } from "lucide-react";
+import { formatEther } from "viem";
 
-export default function Course({ address }: { address: string }) {
-    const { data: course, isPending, error } = useReadContract({
+interface CourseDto {
+    title: string;
+    slug: string;
+    description: string;
+    category: string;
+    price: bigint; // Assuming price is returned as uint256
+}
+
+interface CourseProps {
+    address: string;
+}
+
+export default function Course({ address }: CourseProps) {
+    const { data, isLoading, error } = useReadContract({
         address: address as `0x${string}`,
         abi: courseAbi,
-        functionName: "title"
-    })
-    console.log(address, course, isPending);
+        functionName: "get",
+    });
+
+    // Type assertion to CourseDto
+    const course = data as CourseDto | undefined;
 
     if (error)
         return (
-            <div>
-                {/* skeleton that shows that an error occurred */}
+            <div
+                className="bg-red-100 border border-red-400 text-red-700 px-6 py-4 rounded relative"
+                role="alert"
+            >
+                <strong className="font-bold">Error:</strong>
+                <span className="block sm:inline">
+                    {error.message || "An unexpected error occurred."}
+                </span>
             </div>
         );
-    if (isPending)
+
+    if (isLoading)
         return (
-            <div>
-                {/* plese create me a skeleton of the component here */}
-            </div>
-        )
-    return (
-        <div className="bg-gray-700/50 rounded-lg p-4">
-            <div className="flex justify-between items-start mb-4">
-            <div>
-                <h3 className="font-semibold mb-1">{course.title}</h3>
-                <div className="flex items-center text-sm text-gray-400">
-                <Users className="w-4 h-4 mr-1" />
-                {course.students} students
-                <span className="mx-2">•</span>
-                <DollarSign className="w-4 h-4 mr-1" />
-                {course.revenue}
+            <div className="bg-gray-700/50 rounded-lg p-6 animate-pulse">
+                <div className="flex justify-between items-start mb-4">
+                    <div className="space-y-3 w-full">
+                        {/* Title and Category Skeleton */}
+                        <div className="flex items-center space-x-2">
+                            <div className="h-6 bg-gray-500 rounded w-3/4"></div>
+                            <div className="h-4 bg-gray-500 rounded-full w-24"></div>
+                        </div>
+                        {/* Description Skeleton */}
+                        <div className="h-4 bg-gray-500 rounded w-full"></div>
+                        {/* Price Skeleton */}
+                        <div className="flex items-center text-sm text-gray-400 space-x-2">
+                            <div className="w-4 h-4 bg-gray-500 rounded"></div>
+                            <div className="w-16 h-4 bg-gray-500 rounded"></div>
+                        </div>
+                    </div>
+                    {/* Settings Button Skeleton */}
+                    <div className="w-5 h-5 bg-gray-500 rounded"></div>
                 </div>
             </div>
-            <button className="text-indigo-400 hover:text-indigo-300">
-                <Settings className="w-5 h-5" />
-            </button>
-            </div>
-            <div className="w-full bg-gray-600 rounded-full h-2">
-            <div
-                className="bg-indigo-500 rounded-full h-2"
-                style={{ width: `${course.progress}%` }}
-            />
+        );
+
+    return (
+        <div className="bg-gray-700/50 rounded-lg p-6">
+            <div className="flex justify-between items-start">
+                <div className="space-y-2 w-full">
+                    {/* Title and Category */}
+                    <div className="flex items-center space-x-2">
+                        {/* Course Title */}
+                        <h3 className="font-semibold text-lg">{course?.title}</h3>
+
+                        {/* Category as a Pill-Style Tag */}
+                        <span className="inline-block bg-indigo-600 text-white text-xs px-3 py-1 rounded-full">
+                            {course?.category}
+                        </span>
+                    </div>
+
+                    {/* Course Description */}
+                    <p className="text-gray-300 text-sm">
+                        {course?.description}
+                    </p>
+
+                    {/* Price */}
+                    <div className="flex items-center text-sm text-gray-400">
+                        <DollarSign className="w-4 h-4 mr-1" />
+                        <span>{formatEther(course?.price || 0n)} ETH</span>
+                    </div>
+                </div>
+
+                {/* Settings Button */}
+                <button className="text-indigo-400 hover:text-indigo-300 p-1">
+                    <Settings className="w-5 h-5" />
+                </button>
             </div>
         </div>
     );
