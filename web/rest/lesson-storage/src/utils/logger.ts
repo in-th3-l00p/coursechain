10564ae61aac3b3956edfc0712b4084fb23890f9
in-th3-l00p <indianucode0303@gmail.com>
2024-11-14 10:12:47 +0200

    added lesson-storage rest service, and restructured the project

---
+++ web/rest/lesson-storage/src/utils/logger.ts
@@ -0,0 +1,27 @@
+import winston from "winston"
+
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({
+            filename: "./logs/error.log",
+            level: "error"
+        }),
+        new winston.transports.File({
+            filename: "./logs/info.log",
+            level: "info"
+        })
+    ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }
+
+export default logger;
